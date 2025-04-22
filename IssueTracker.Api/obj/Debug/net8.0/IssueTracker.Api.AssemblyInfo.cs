031e1a2a3f20211274e8574852f99cffb042c67f
Elshama Mokoena <elshamamokoena@gmail.com>
2025-04-23 00:32:01 +0200

    deleted React template

--- IssueTracker.Api/obj/Debug/net8.0/IssueTracker.Api.AssemblyInfo.cs
+++ IssueTracker.Api/obj/Debug/net8.0/IssueTracker.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IssueTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd4ea79af4fc2069179731a3e6ad13f4382660f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e7a4235a8afacfd6208300dadb9536b42aacf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IssueTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IssueTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
